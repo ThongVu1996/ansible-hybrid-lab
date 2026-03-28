420dd4938e9e592cdbcf8269e959d9c17e5a1c19
thongvu <anhthongvu1996@gmail.com[D>
2026-03-28 17:31:40 +0700

    fixbug: refactor code

--- jenkins/create-vm/outputs.tf
+++ jenkins/create-vm/outputs.tf
@@ -1,10 +1,9 @@
-output "vm_ip" {
-  description = "Địa chỉ IP cố định của Jenkins Server"
-  value       = module.jenkins_vm.vm_ip
+output "vm_ips" {
+  description = "Danh sách địa chỉ IP cố định của các máy ảo Jenkins"
+  value       = module.jenkins_vm.vm_ips
 }
 
-output "tailscale_login" {
-  description = "Hướng dẫn kết nối với máy chủ qua Tailscale"
-  value       = module.jenkins_vm.tailscale_login
+output "tailscale_ssh_commands" {
+  description = "Các lệnh SSH để truy cập qua mạng Tailscale"
+  value       = module.jenkins_vm.tailscale_ssh_commands
 }
-
