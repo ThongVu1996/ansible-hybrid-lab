8e60a62f088aec7c015dd073bafb2d7028db2310
thongvu <anhthongvu1996@gmail.com[D>
2026-04-01 22:10:10 +0700

    feat: oidc hcp vault

--- crete-vault-cluster/main.tf
+++ crete-vault-cluster/main.tf
@@ -7,23 +7,30 @@ terraform {
   }
 }
 
+variable "hcp_hvn_id"     { type = string }
+variable "hcp_hvn_cloud_provider" { type = string }
+variable "hcp_hvn_region"  { type = string } 
+variable "hcp_hvn_cidr_block"     { type = string }
+variable "hcp_vault_cluster_cluster_id"       { type = string }
+variable "hcp_vault_cluster_tier"     { type = string }
+
 provider "hcp" {
   # Terraform sẽ tự đọc từ biến môi trường HCP_CLIENT_ID và HCP_CLIENT_SECRET
 }
 
-# 1. Tạo mạng nội bộ HVN (Cực kỳ quan trọng để Vault chạy)
+# 1. Tạo mạng nội bộ HVN (Dùng biến đã khai báo)
 resource "hcp_hvn" "main_hvn" {
-  hvn_id         = "ansible-lab-hvn"
-  cloud_provider = "aws"
-  region         = "ap-southeast-1"
-  cidr_block     = "10.0.0.0/16"
+  hvn_id         = var.hcp_hvn_id
+  cloud_provider = var.hcp_hvn_cloud_provider
+  region         = var.hcp_hvn_region
+  cidr_block     = var.hcp_hvn_cidr_block
 }
 
-# 2. Khởi tạo Vault Cluster (Gói Dev - Public Endpoint)
+# 2. Khởi tạo Vault Cluster (Dùng biến đã khai báo)
 resource "hcp_vault_cluster" "vault_cluster" {
-  cluster_id      = "ansible-vault-cluster"
+  cluster_id      = var.hcp_vault_cluster_cluster_id
   hvn_id          = hcp_hvn.main_hvn.hvn_id
-  tier            = "dev"
+  tier            = var.hcp_vault_cluster_tier
   public_endpoint = true
 }
 
